9be0453da079ce1febe67996ebe7ca762bb39f0b
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-02-24 12:07:59 -0600

    Create PlatilloPedidoBean.java

---
+++ src/java/alfaware/pedidodeplatillosnuevo/managedbeans/PlatilloPedidoBean.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alfaware.pedidodeplatillosnuevo.managedbeans;
+
+import alfaware.pedidodeplatillosnuevo.controllers.PlatilloPedidoController;
+import alfaware.pedidodeplatillosnuevo.entities.Pedido;
+import alfaware.pedidodeplatillosnuevo.entities.Platillo;
+import alfaware.pedidodeplatillosnuevo.entities.Platillopedido;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author ALFA-AS3-1A6600
+ *
+ */
+@ManagedBean
+@Named(value = "platilloPedidoBean")
+@SessionScoped
+public class PlatilloPedidoBean implements Serializable {
+
+    private String id;
+    private Platillo idPlatillo;
+    private Pedido idPedido;
+    private int cantidad;
+
+    private PlatilloPedidoController platillopedidoController;
+    Platillopedido pp = new Platillopedido();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Platillo getIdPlatillo() {
+        return idPlatillo;
+    }
+
+    public void setIdPlatillo(Platillo idPlatillo) {
+        this.idPlatillo = idPlatillo;
+    }
+
+    public Pedido getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Pedido idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String guardar() {
+        pp.setIdPlatillo(idPlatillo);
+        pp.setIdPedido(idPedido);
+        pp.setCantidad(cantidad);
+        this.id = pp.getId();
+        this.platillopedidoController.add(pp);
+        return "platillopedido";
+    }
+
+    public List<Platillopedido> getPlatillopedidos() {
+        Map<String, Object> platped = this.platillopedidoController.get(pp);
+        List<Platillopedido> result = new ArrayList(platped.values());
+        return result;
+    }
+
+    /**
+     * Creates a new instance of PlatilloPedidoBean
+     */
+    public PlatilloPedidoBean() {
+    }
+
+}

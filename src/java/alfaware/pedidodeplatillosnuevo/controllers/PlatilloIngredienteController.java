cf2157c8bf8963c6d1a9ec34bb05b64bf99f0553
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-02-24 12:07:12 -0600

    Create PlatilloIngredienteController.java

---
+++ src/java/alfaware/pedidodeplatillosnuevo/controllers/PlatilloIngredienteController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alfaware.pedidodeplatillosnuevo.controllers;
+
+import alfaware.alfawareutils.utils.ControllerUtils;
+
+/**
+ *
+ * @author ALFA-AS3-1A6600
+ */
+public class PlatilloIngredienteController extends ControllerUtils{
+    
+    public PlatilloIngredienteController(){
+        super();
+    }
+    
+}
+

6a71ffd12bd2da47e39acbd0887e4162d8985e13
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-02-24 12:07:20 -0600

    Create Pedido.java

---
+++ src/java/alfaware/pedidodeplatillosnuevo/entities/Pedido.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alfaware.pedidodeplatillosnuevo.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author ALFA-AS3-1A6600
+ */
+@Entity
+@Table(name = "pedido")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Pedido.findAll", query = "SELECT p FROM Pedido p")
+    , @NamedQuery(name = "Pedido.findById", query = "SELECT p FROM Pedido p WHERE p.id = :id")})
+public class Pedido implements Serializable {
+
+    //private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "id")
+    private String id;
+    @Lob
+    @Size(max = 16777215)
+    @Column(name = "fecha")
+    private String fecha;
+    @OneToMany(mappedBy = "idPedido")
+    private Collection<Platillopedido> platillopedidoCollection;
+    @JoinColumn(name = "idPersona", referencedColumnName = "id")
+    @ManyToOne
+    private Persona idPersona;
+
+    public Pedido() {
+    }
+
+    public Pedido(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    @XmlTransient
+    public Collection<Platillopedido> getPlatillopedidoCollection() {
+        return platillopedidoCollection;
+    }
+
+    public void setPlatillopedidoCollection(Collection<Platillopedido> platillopedidoCollection) {
+        this.platillopedidoCollection = platillopedidoCollection;
+    }
+
+    public Persona getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Persona idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pedido)) {
+            return false;
+        }
+        Pedido other = (Pedido) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "alfaware.pedidodeplatillosnuevo.entities.Pedido[ id=" + id + " ]";
+    }
+    
+}

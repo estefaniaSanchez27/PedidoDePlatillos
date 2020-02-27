41337e95fea999b622f88434cc5816adef1d0a71
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-02-27 11:23:16 -0600

    Update Ingrediente.java

--- src/java/alfaware/pedidodeplatillosnuevo/entities/Ingrediente.java
+++ src/java/alfaware/pedidodeplatillosnuevo/entities/Ingrediente.java
@@ -6,49 +6,18 @@
 package alfaware.pedidodeplatillosnuevo.entities;
 
 import java.io.Serializable;
-import java.util.Collection;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
+
 
 /**
  *
  * @author ALFA-AS3-1A6600
  */
-@Entity
-@Table(name = "ingrediente")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Ingrediente.findAll", query = "SELECT i FROM Ingrediente i")
-    , @NamedQuery(name = "Ingrediente.findById", query = "SELECT i FROM Ingrediente i WHERE i.id = :id")
-    , @NamedQuery(name = "Ingrediente.findByNombre", query = "SELECT i FROM Ingrediente i WHERE i.nombre = :nombre")
-    , @NamedQuery(name = "Ingrediente.findByCostoGramo", query = "SELECT i FROM Ingrediente i WHERE i.costoGramo = :costoGramo")})
 public class Ingrediente implements Serializable {
 
     //private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 50)
-    @Column(name = "id")
     private String id;
-    @Size(max = 50)
-    @Column(name = "nombre")
     private String nombre;
-    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
-    @Column(name = "costoGramo")
     private Double costoGramo;
-    @OneToMany(mappedBy = "idIngrediente")
-    private Collection<Platilloingrediente> platilloingredienteCollection;
 
     public Ingrediente() {
     }
@@ -81,38 +50,9 @@ public class Ingrediente implements Serializable {
         this.costoGramo = costoGramo;
     }
 
-    @XmlTransient
-    public Collection<Platilloingrediente> getPlatilloingredienteCollection() {
-        return platilloingredienteCollection;
-    }
-
-    public void setPlatilloingredienteCollection(Collection<Platilloingrediente> platilloingredienteCollection) {
-        this.platilloingredienteCollection = platilloingredienteCollection;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Ingrediente)) {
-            return false;
-        }
-        Ingrediente other = (Ingrediente) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
     @Override
     public String toString() {
-        return "alfaware.pedidodeplatillosnuevo.entities.Ingrediente[ id=" + id + " ]";
+        return id;
     }
     
 }

157160e093839ef8ae2a9a06cbf7a72a6cf641e1
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-02-27 11:23:10 -0600

    Update PlatilloPedidoController.java

--- src/java/alfaware/pedidodeplatillosnuevo/controllers/PlatilloPedidoController.java
+++ src/java/alfaware/pedidodeplatillosnuevo/controllers/PlatilloPedidoController.java
@@ -6,15 +6,56 @@
 package alfaware.pedidodeplatillosnuevo.controllers;
 
 import alfaware.alfawareutils.utils.ControllerUtils;
+import alfaware.pedidodeplatillosnuevo.entities.Pedido;
+import alfaware.pedidodeplatillosnuevo.entities.Platillo;
+import alfaware.pedidodeplatillosnuevo.entities.Platillopedido;
+import java.util.Map;
 
 /**
  *
  * @author ALFA-AS3-1A6600
  */
-public class PlatilloPedidoController extends ControllerUtils{
-    
-    public PlatilloPedidoController(){
+public class PlatilloPedidoController extends ControllerUtils {
+
+    public PlatilloPedidoController() {
         super();
     }
+
+    public static Map<String, Object> select(Platillopedido plaped) {
+        Map<String, Object> map = get(plaped);
+
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            Platillopedido platilloped = ((Platillopedido) entry.getValue());
+            Map<String, Object> pe = PedidoController.select(new Pedido(platilloped.getIdPedido()));
+            Map<String, Object> pl = PlatilloController.select(new Platillo(platilloped.getIdPlatillo()));
+            if (!pe.isEmpty() && !pl.isEmpty()) {
+                platilloped.setIdPedido(String.valueOf(pe.get(platilloped.getIdPedido())));
+                platilloped.setIdPlatillo(String.valueOf(pl.get(platilloped.getIdPlatillo())));
+            }
+        }
+
+        return map;
+    }
     
+    public void insert(Platillopedido plaped,String idPlatillo,String idPedido,int cantidad){
+        plaped.setIdPlatillo(idPlatillo);
+        plaped.setIdPedido(idPedido);
+        plaped.setCantidad(cantidad);
+        plaped.getId();
+        add(plaped);
+    }
+    
+    public void update(Platillopedido plaped,String id,String idPlatillo,String idPedido,int cantidad){
+        plaped.setId(id);
+        plaped.setIdPlatillo(idPlatillo);
+        plaped.setIdPedido(idPedido);
+        plaped.setCantidad(cantidad);
+        modify(plaped);
+    }
+    
+    public void delete(Platillopedido plaped,String id){
+        plaped.setId(id);
+        remove(plaped);
+    }
+
 }

fba31f4313fd8f359cf63cede85d010b5306242e
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-02-27 11:23:21 -0600

    Update Persona.java

--- src/java/alfaware/pedidodeplatillosnuevo/entities/Persona.java
+++ src/java/alfaware/pedidodeplatillosnuevo/entities/Persona.java
@@ -6,60 +6,25 @@
 package alfaware.pedidodeplatillosnuevo.entities;
 
 import java.io.Serializable;
-import java.util.Collection;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
+import java.util.List;
 
 /**
  *
  * @author ALFA-AS3-1A6600
  */
-@Entity
-@Table(name = "persona")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Persona.findAll", query = "SELECT p FROM Persona p")
-    , @NamedQuery(name = "Persona.findById", query = "SELECT p FROM Persona p WHERE p.id = :id")
-    , @NamedQuery(name = "Persona.findByNombre", query = "SELECT p FROM Persona p WHERE p.nombre = :nombre")
-    , @NamedQuery(name = "Persona.findByTelefono", query = "SELECT p FROM Persona p WHERE p.telefono = :telefono")})
 public class Persona implements Serializable {
 
     //private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 50)
-    @Column(name = "id")
     private String id;
-    @Size(max = 50)
-    @Column(name = "nombre")
     private String nombre;
-    @Size(max = 10)
-    @Column(name = "telefono")
     private String telefono;
-    @Lob
-    @Size(max = 65535)
-    @Column(name = "direccion")
     private String direccion;
-    @OneToMany(mappedBy = "idPersona")
-    private Collection<Pedido> pedidoCollection;
 
     public Persona() {
     }
 
     public Persona(String id) {
-        this.id = id;
+        this.id=id;
     }
 
     public String getId() {
@@ -94,38 +59,19 @@ public class Persona implements Serializable {
         this.direccion = direccion;
     }
 
-    @XmlTransient
-    public Collection<Pedido> getPedidoCollection() {
-        return pedidoCollection;
+    /*public PedidoPersona getPedidopersona() {
+        return pedidopersona;
     }
 
-    public void setPedidoCollection(Collection<Pedido> pedidoCollection) {
-        this.pedidoCollection = pedidoCollection;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Persona)) {
-            return false;
-        }
-        Persona other = (Persona) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
+    public void setPedidopersona(PedidoPersona pedidopersona) {
+        this.pedidopersona = pedidopersona;
+    }*/
+    
+    
+    
     @Override
     public String toString() {
-        return "alfaware.pedidodeplatillosnuevo.entities.Persona[ id=" + id + " ]";
+        return id;
     }
-    
+
 }

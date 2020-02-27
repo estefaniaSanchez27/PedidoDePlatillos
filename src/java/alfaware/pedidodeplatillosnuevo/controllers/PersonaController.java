3848d05443570c40cc7a33b3d0a2b701b089fa7d
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-02-27 11:23:02 -0600

    Update PersonaController.java

--- src/java/alfaware/pedidodeplatillosnuevo/controllers/PersonaController.java
+++ src/java/alfaware/pedidodeplatillosnuevo/controllers/PersonaController.java
@@ -13,15 +13,35 @@ import java.util.Map;
  *
  * @author ALFA-AS3-1A6600
  */
-public class PersonaController extends ControllerUtils{
-    
-    public PersonaController(){
+public class PersonaController extends ControllerUtils {
+
+    public PersonaController() {
         super();
     }
-    
-    public static Map<String,Object> select(Persona per){
-        Map<String,Object> map = get(per);
+
+    public static Map<String, Object> select(Persona per) {
+        Map<String, Object> map = get(per);
         return map;
     }
-}
 
+    public void insert(Persona per, String nombre, String telefono, String direccion) {
+        per.setNombre(nombre);
+        per.setTelefono(telefono);
+        per.setDireccion(direccion);
+        per.getId();
+        add(per);
+    }
+
+    public void update(Persona per, String id, String nombre, String telefono, String direccion) {
+        per.setId(id);
+        per.setNombre(nombre);
+        per.setTelefono(telefono);
+        per.setDireccion(direccion);
+        modify(per);
+    }
+
+    public void delete(Persona per, String id) {
+        per.setId(id);
+        remove(per);
+    }
+}

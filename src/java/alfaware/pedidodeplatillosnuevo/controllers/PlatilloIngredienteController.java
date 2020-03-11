d7fc3af3445e3f9395561e77c17eff00499d1bfa
estefaniaSanchez27 <60798289+estefaniaSanchez27@users.noreply.github.com>
2020-03-11 10:20:32 -0600

    Update PlatilloIngredienteController.java

--- src/java/alfaware/pedidodeplatillosnuevo/controllers/PlatilloIngredienteController.java
+++ src/java/alfaware/pedidodeplatillosnuevo/controllers/PlatilloIngredienteController.java
@@ -9,6 +9,7 @@ import alfaware.alfawareutils.utils.ControllerUtils;
 import alfaware.pedidodeplatillosnuevo.entities.Ingrediente;
 import alfaware.pedidodeplatillosnuevo.entities.Platillo;
 import alfaware.pedidodeplatillosnuevo.entities.Platilloingrediente;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -37,10 +38,9 @@ public class PlatilloIngredienteController extends ControllerUtils {
         return map;
     }
 
-    public void insert(Platilloingrediente plaing, String idIngrediente, String idPlatillo, Double gramos) {
+    public void insert(Platilloingrediente plaing, String idIngrediente, String idPlatillo) {
         plaing.setIdIngrediente(idIngrediente);
         plaing.setIdPlatillo(idPlatillo);
-        plaing.setGramos(gramos);
         plaing.getId();
         add(plaing);
     }
@@ -53,9 +53,40 @@ public class PlatilloIngredienteController extends ControllerUtils {
         modify(plaing);
     }
 
-    public void delete(Platilloingrediente plaing, String id) {
-        plaing.setId(id);
-        remove(plaing);
+    public void delete(Ingrediente i, String idPlatillo, Platilloingrediente plaing) {
+        String idIngrediente = i.getId();
+        Map<String, Object> platillosingredientes = select(plaing);
+        for (Map.Entry<String, Object> entry : platillosingredientes.entrySet()) {
+            Platilloingrediente platilloing = ((Platilloingrediente) entry.getValue());
+            if (idPlatillo == null ? platilloing.getIdPlatillo() == null : idPlatillo.equals(platilloing.getIdPlatillo())) {
+                if (idIngrediente == null ? platilloing.getIdIngrediente() == null : idIngrediente.equals(platilloing.getIdIngrediente())) {
+                    String id = platilloing.getId();
+                    plaing.setId(id);
+                    remove(plaing);
+                }
+            }
+        }
     }
 
+    public static Map<String, Object> obtenerIngredientesdePlatillo(Platillo pla, Ingrediente ing, Platilloingrediente plaing, String id) {
+        pla.setId(id);
+        Map<String, Object> platillosingredientes = select(plaing);
+        Map<String, Object> ingredientes = get(ing);
+        Map<String, Object> map = new HashMap<>();
+        for (Map.Entry<String, Object> entry : platillosingredientes.entrySet()) {
+            Platilloingrediente platilloing = ((Platilloingrediente) entry.getValue());
+            if (id == null ? platilloing.getIdPlatillo() == null : id.equals(platilloing.getIdPlatillo())) {
+                if (ingredientes.containsKey(platilloing.getIdIngrediente())) {
+                    Map<String, Object> i = IngredienteController.select(new Ingrediente(platilloing.getIdIngrediente()));
+                    map.putAll(i);
+                }
+            }
+        }
+        return map;
+    }
+
+    //public static Map<String,Object> obtenerIngrediente(Ingrediente i){
+    //Map<String,Object> ingredientes = get(i);
+    //return ingredientes;
+    //}
 }
